b7a123263d10a08a00b5f0df070982427dbd72c2
HungIT0312 <nguyenhungit0312@gmail.com>
2023-05-20 09:10:50 +0700

    new

--- src/components/genres/Genres.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-const Genres = (props) => {
-  const [genre, setGenre] = useState(props.genre);
-  return <div className="content__genre-item">{genre.name}</div>;
-};
-
-export default Genres;
